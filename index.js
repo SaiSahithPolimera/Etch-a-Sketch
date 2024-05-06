092378ca8901bc27aa9704dbd916015f43aaefec
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-05-06 19:06:12 +0530

    Add border, background color features

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const container = document.getElementById("container");
+  const container = document.getElementById("container");
 const colorPicker = document.getElementById("colorPicker");
 const randomPaint = document.getElementById("random-color");
 const clear = document.getElementById("clear");
 const backgroundPicker = document.getElementById("backgroundPicker");
 const eraser = document.getElementById("eraser");
+const border = document.getElementById("borders");
+let borderFlag = 0;
+let eraserColor = "#fff";
 
 container.style.display = "flex";
 container.style.width = "450px";
@@ -24,16 +27,31 @@ function showValue() {
 createRows(16, 16);
 setPixelColor();
 
+border.addEventListener("click", () => {
+  const selector = document.querySelectorAll('div[class^="row-"]');
+  if (borderFlag == 0) {
+    selector.forEach((pixel) => {
+      pixel.style.border = "#fff";
+      borderFlag = 1;
+    });
+  } else {
+    selector.forEach((pixel) => {
+      pixel.style.border = "1px solid black";
+      borderFlag = 0;
+    });
+  }
+});
+
 function createRows(rowCount, colCount) {
   elementCounter = 0;
-  let width =  ((450 - rowCount) / rowCount - 0.5)  + "px";
-  let height =  ((450 - colCount) / colCount - 0.5)   + "px";
+  let width = (450 - rowCount) / rowCount - 0.5 + "px";
+  let height = (450 - colCount) / colCount - 0.5 + "px";
   for (rowCounter = 0; rowCounter < rowCount; rowCounter++) {
     const cols = document.createElement("div");
     for (colCounter = 0; colCounter < colCount; colCounter++) {
       const col = document.createElement("div");
       col.classList.add("row-" + elementCounter);
-      col.style.width =  width;
+      col.style.width = width;
       col.style.height = height;
       col.style.border = "1px solid black";
       cols.appendChild(col);
@@ -69,14 +87,14 @@ function setPixelColor() {
       pixel.style.backgroundColor = getColorCode();
     });
     pixel.addEventListener("dblclick", () => {
-      pixel.style.backgroundColor = "#fff";
+      pixel.style.backgroundColor = eraserColor;
     });
     pixel.addEventListener("mouseover", () => {
       color = String(getRandomColor());
       pixel.style.backgroundColor = getRandomColor();
-      })
     });
-  }
+  });
+}
 
 randomPaint.addEventListener("click", () => {
   randomPaint.style.backgroundColor = String(getRandomColor());
@@ -93,8 +111,8 @@ eraser.addEventListener("click", () => {
   const selector = document.querySelectorAll('div[class^="row"]');
   selector.forEach((pixel) => {
     pixel.addEventListener("mouseenter", () => {
-      pixel.style.backgroundColor = "#fff";
-    })
+      pixel.style.backgroundColor = eraserColor;
+    });
   });
 });
 
@@ -110,26 +128,15 @@ clear.addEventListener("click", () => {
   selector.forEach((pixel) => {
     pixel.style.backgroundColor = "";
   });
-
 });
 
-// eraser.addEventListener("mouseenter", () => {
-//   const selector = document.querySelectorAll('div[class^="row-"]');
-//   selector.forEach((pixel)=> {
-//     pixel.style.backgroundColor = "";
-//   }) 
-// })
-
 function setBackgroundColor() {
-  // const selector = document.querySelectorAll('div[class^="row-"]');
-  // selector.forEach((pixel) => {
-  //   let pixelStyle = window.getComputedStyle(pixel)
-  //   if(pixelStyle.backgroundColor === 'rgba(0, 0, 0, 0)') {
-  //     let backgroundStyle = window.getComputedStyle(backgroundPicker);
-  //     alert(backgroundStyle.backgroundColor);
-  //     pixel.style.backgroundColor = backgroundStyle.backgroundColor;
-  //   }
-  // })
+  const selector = document.querySelectorAll('div[class^="row-"]');
+  selector.forEach((pixel) => {
+    pixel.style.transition = "0.9s transition ease-in-out";
+    pixel.style.backgroundColor = backgroundPicker.value;
+    eraserColor = backgroundPicker.value;
+  })
 }
 
 backgroundPicker.addEventListener("keydown", setBackgroundColor);
